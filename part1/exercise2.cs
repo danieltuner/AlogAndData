ede772d598a7b662f7ea2d807521d6f781cea4ce
Daniel Tunér <tuner.daniel@hotmail.com>
2020-05-09 21:49:07 +0300

    first try

--- part1/exercise2.cs
+++ part1/exercise2.cs
@@ -1,10 +1,42 @@
+using System;
+using System.Collections.Generic;
+
 namespace part1
 {
   public class Substrings
   {
     public int Calculate(string a, string b)
     {
-      return 0;
+      int one = a.Length;
+      int two = b.Length;
+      int result = 0;
+
+      for (int i = 0; i <= one - two; i++)
+      {
+        int three;
+        for (three = 0;three < one; three++)
+          if (b[i + three] !=a[three])
+          {
+            break;
+          }
+        if (three == one)
+        {
+          result++;
+          three = 0;
+        }
+        return result;
+      }
+      // how many times is b in a
+
+      /*
+      amountOfHits = 0
+      for character in a, until a.Length-b.Legth
+        if character == b index =
+          if a in next index == b in next index
+            until b ends
+            amountOfHits++
+      return the amountOfHits
+      */
     }
   }
 }
