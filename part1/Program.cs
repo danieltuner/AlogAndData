0b1bf2f523a694079e92bf4b58af8102fc495f55
Daniel Tunér <tuner.daniel@hotmail.com>
2020-05-09 20:44:16 +0300

    program

--- part1/Program.cs
+++ part1/Program.cs
@@ -12,6 +12,21 @@ namespace part1
             Console.WriteLine(num.Sum(3)); // 3
             Console.WriteLine(num.Sum(999999999)); // 81
 
+            Substrings subs = new Substrings();
+            subs.Calculate("aybabtu", "bab"); // 1
+            subs.Calculate("aaaaa", "aa"); // 4
+            subs.Calculate("monkey", "banana"); // 0
+
+            Tables t = new Tables();
+            Console.WriteLine(t.Calculate(new int[] {1,2,3,2})); // 18
+            Console.WriteLine(t.Calculate(new int[] {5})); // 5
+            Console.WriteLine(t.Calculate(new int[] {4,2,9,1,9,2,5})); // 323
+            
+            LuckyNumbers luck = new LuckyNumbers();
+            Console.WriteLine(luck.Calculate(1,10)); // 2
+            Console.WriteLine(luck.Calculate(123,321)); // 0
+            Console.WriteLine(luck.Calculate(1,1000000)); // 126
+
 
         }
     }
