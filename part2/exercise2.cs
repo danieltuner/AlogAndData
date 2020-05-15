7f4f29b09ceaf0bdd39d7b074b97e443561027d3
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-15 13:16:03 +0300

    first try

---
+++ part2/exercise2.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace part2
+{
+
+    public class LongestRepetition
+    {
+        public int Calculate(int[] t)
+        {
+            int repetition = 0;
+            for (int i = t[0]; i < t.Length -1; i++)
+            {
+                if (t[i] == t[i-1])
+                {
+                    repetition++;
+                }
+            }
+            return repetition;
+        }
+    }
+}
