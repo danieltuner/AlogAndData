a8023851c16cb0069aa50b7f714041c4d0040e03
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-18 13:06:50 +0300

    5 base

---
+++ part2/exercise5.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace part2
+{
+
+    public class Split
+    {
+        public int Calculate(int[] t)
+        {       
+            
+        }
+        
+            
+    }
+}
