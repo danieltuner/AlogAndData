77308c5011866b0769a9b6c9857f9cc3097e9343
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-18 12:53:11 +0300

    progg

--- part2/Program.cs
+++ part2/Program.cs
@@ -36,9 +36,9 @@ namespace part2
             Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2            */
 
             Revolutions r = new Revolutions();
-            Console.WriteLine(m.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
-            Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-            Console.WriteLine(m.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
+            Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
+            Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+            Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
 
 
         }
--- part2/exercise4.cs
+++ part2/exercise4.cs
@@ -8,7 +8,18 @@ namespace part2
     {
         public int Calculate(int[] t)
         {       
-            
+            int NrOfRuns = 0;
+            for (int i = 1; i <= t.Length; i++)
+            {
+                for (int j = 0; j <= t.Length; j++)
+                {
+                    if (t[i] == NrOfRuns)
+                    {
+                        NrOfRuns++;
+                    }
+                }
+            }
+            return NrOfRuns;
         }
         
             
