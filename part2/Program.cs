4fe80a91e8b7ad75b94dc6bd82614fc32375f1e1
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-18 12:21:24 +0300

    ex 3 done

--- part2/Program.cs
+++ part2/Program.cs
@@ -33,7 +33,7 @@ namespace part2
             Changes m = new Changes();
             Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
             Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1, 1, })); // 2            */
+            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2            */
 
 
         }
--- part2/exercise3.cs
+++ part2/exercise3.cs
@@ -9,17 +9,20 @@ namespace part2
         public int Calculate(int[] t)
         {
             int change = 0;
-            for (int i = 1; i < t.Length -1; i++)
+            for (int i = 1; i <= t.Length -1; i++)
             {
                 if (t[i] == t[i-1])
                 {                    
                     change++;
-                    t[i] = t[i-1] + t[i+1] +1;
-                }
-                if (t[i] == t[i-1])
-                {
-                    change++;
-                }
+                    if (i < t.Length -1)
+                    {
+                        t[i] = t[i-1] + t[i + 1] + 1;
+                    }
+                    else
+                    {
+                        t[i] = t[i-1] - t[i];
+                    }
+                }                
             }
             return change;
             
