8f10e09b2e91c49a042157c181676cc1f024488d
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-18 10:19:25 +0300

    base

---
+++ part2/exercise3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace part2
+{
+
+    public class Changes
+    {
+        public int Calculate(int[] t)
+        {
+            
+        }
+        
+            
+    }
+}
