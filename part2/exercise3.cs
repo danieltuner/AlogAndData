4f41145100a802d1b5ce0d19156de830191008f5
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-18 10:52:12 +0300

    test

--- part2/exercise3.cs
+++ part2/exercise3.cs
@@ -8,15 +8,16 @@ namespace part2
     {
         public int Calculate(int[] t)
         {
-            int sum = 0;
-            for (int i = t[0]; i < t.Length -1; i++)
+            int change = 0;
+            for (int i = 1; i < t.Length -1; i++)
             {
                 if (t[i] == t[i-1])
-                {
-                    sum++;
+                {                    
+                    change++;
+                    t[i] = t[i-1] + t[i+1];
                 }
             }
-            return sum;
+            return change;
         }
         
             
