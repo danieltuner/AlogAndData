b8ed22fb8deedb69e6f96e223e2cf837dabff69c
Daniel Tunér <tuner.daniel@hotmail.com>
2020-05-19 14:35:02 +0300

    ex 5 done

--- part2/exercise5.cs
+++ part2/exercise5.cs
@@ -6,11 +6,29 @@ namespace part2
 
     public class Split
     {
-        /* ublic int Calculate(int[] t)
+        public int Calculate(int[] t)
         {       
-            
-        }
-        
-            */
+            int leftSum = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                leftSum = leftSum + t[i];
+            }
+
+            int rightSum = 0;
+            int count = 0;
+
+            for (int i = t.Length - 1; i > 0; i--)
+            {
+              
+                rightSum = rightSum + t[i];               
+                leftSum = leftSum - t[i];
+
+                if (rightSum == leftSum)
+                {
+                    count++;                                         
+                }                                                            
+            }
+            return count;    
+        }                    
     }
 }
