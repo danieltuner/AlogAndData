be0864295fb07c5f423bee299f617c400f601a49
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-22 11:41:22 +0300

    progg

--- part3/Program.cs
+++ part3/Program.cs
@@ -17,14 +17,22 @@ namespace part3
             Console.WriteLine(s.Calculate(new int[] { 1, 10, 100 })); // 9
             Console.WriteLine(s.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 0
             Console.WriteLine(s.Calculate(Randomizer(10))); // depends on random */
-
+            /*
             Sorting s = new Sorting();
             int[] sortMe = Randomizer(100);
             int[] sortMeLarge = Randomizer(1000000);
             s.QuickSort(sortMe);
             //s.MergeSort(sortMe);
             s.QuickSort(sortMeLarge);
-            //s.MergeSort(sortMeLarge);
+            s.MergeSort(sortMeLarge); */
+
+            BinarySearch b = new BinarySearch();
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5 }, 2)); // false
+            Console.WriteLine(b.Find((new int[] { 0, 0 }, 0)); // true
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4,, 2, 3 }, 2)); // true
+            Console.WriteLine(b.Find((new int[] { 0 }, 0)); // true
+            onsole.WriteLine(s.Calculate(Randomizer(100000), 3)); // ?
+
 
         }
         public static int[] Randomizer(int n)
