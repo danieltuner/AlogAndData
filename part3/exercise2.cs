3d07a465b76332f7b86e4f2625171c953d2fcc95
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-22 10:34:13 +0300

    ex 2 done

--- part3/exercise2.cs
+++ part3/exercise2.cs
@@ -6,7 +6,17 @@ namespace part3
     {
         public int Calculate(int[] t)
         {
-            return 0;
+            Array.Sort(t);
+            int diff = int.MaxValue;
+
+            for (int i = 0; i < t.Length - 1; i++)
+            {
+                if (t[i+1] - t[i] < diff)
+                {
+                    diff = t[i + 1] - t[i];
+                }
+            }
+            return diff;
         }
 
         
