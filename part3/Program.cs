04d7fb580e858dc0d368a4f39e66978fd85a78b0
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-22 12:20:58 +0300

    hmm

--- part3/Program.cs
+++ part3/Program.cs
@@ -27,11 +27,11 @@ namespace part3
             s.MergeSort(sortMeLarge); */
 
             BinarySearch b = new BinarySearch();
-            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5 }, 2))); // false
-            Console.WriteLine(b.Find((new int[] { 0, 0 }, 0))); // true
-            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }, 2))); // true
-            Console.WriteLine(b.Find((new int[] { 0 }, 0))); // true
-            Console.WriteLine(s.Calculate(Randomizer(100000), 3)); // ?
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5 }), 2)); // false
+            Console.WriteLine(b.Find((new int[] { 0, 0 }), 0)); // true
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }), 2)); // true
+            Console.WriteLine(b.Find((new int[] { 0 }), 0)); // true
+            //Console.WriteLine(s.Calculate(Randomizer(100000), 3)); // ?
 
 
         }
--- part3/exercise4.cs
+++ part3/exercise4.cs
@@ -6,9 +6,43 @@ namespace part3
     {
         public bool Find(int[] t, int x)
         {
+            int a = 0;
+            int b = t.Length - 1;
+            int k;
+            while (a <= b)
+            {
+                k = (a+b) /2;
+                
+                if (t[k] == x)
+                {
+                    return true;
+                }
+                if (t[k] < x)
+                {
+                    a = k +1;
+                }
+                if (t[k] > x)
+                {
+                    b = k - 1;
+                }
+                
+            }
             return false;
 
         }
+        /*
+        a = 0
+        b = n-1
+        while a <= b
+        k = (a+b)/2
+        if array[k] == x
+            // we found the x
+            break   
+        if array[k] < x
+            a = k+1
+        if array[k] > x
+            b = k-1
+            */
 
 
     }
