99718d0d87af3b070e40a37f4ada15537f476edf
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-22 10:46:21 +0300

    base

--- part3/exercise3.cs
+++ part3/exercise3.cs
@@ -7,11 +7,30 @@ namespace part3
         public void MergeSort(int[] t)
         {
 
+
+
+
+/*sort(a,b)
+  if a == b
+    return
+  k = (a+b)/2
+  sort(a,k)
+  sort(k+1,b)
+  merge(a,k,k+1,b)
+  */
         }
 
         public void QuickSort(int[] t)
         {
-
+            
+/*
+sort(a, b)
+  if a >= b
+    return
+  k = pivot(a,b)
+  sort(a, k-1)
+  sort(k+1, b)
+*/
         }
 
         
