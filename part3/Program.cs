e840446e38ce7120372f5d5cd8e5c03760685dad
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-22 11:44:05 +0300

    progg

--- part3/Program.cs
+++ part3/Program.cs
@@ -29,7 +29,7 @@ namespace part3
             BinarySearch b = new BinarySearch();
             Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5 }, 2)); // false
             Console.WriteLine(b.Find((new int[] { 0, 0 }, 0)); // true
-            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4,, 2, 3 }, 2)); // true
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }, 2)); // true
             Console.WriteLine(b.Find((new int[] { 0 }, 0)); // true
             onsole.WriteLine(s.Calculate(Randomizer(100000), 3)); // ?
 
