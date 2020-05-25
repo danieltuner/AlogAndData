82c6c594f8a63c1a4c829bb40dc0505a8678edec
Daniel Tunér <tuner.daniel@hotmail.com>
2020-05-25 12:47:49 +0300

    ex 5 done

--- part3/exercise5.cs
+++ part3/exercise5.cs
@@ -6,18 +6,30 @@ namespace part3
     {
         public int[] Create(int n, int k)
         {
-            int k = 0;
-            for (int i = 0; i < n - 1; i++)
+            int [] array = new int [n];
+            for (int i = 0; i < n; i++)
             {
-                for ( int j = 0; j < n; j++)
+                array[i] = i + 1;
+            }
+            int inversionsCount = 0;
+            int a = 0;
+            int b = (n - 1);
+
+            while (inversionsCount < k)
+            {
+                int temp = array[b];
+                array[b] = array[b - 1];
+                array[b - 1] = temp;
+
+                inversionsCount++;
+                b--;
+                if ( b == a)
                 {
-                    if (n[i] > n[j])
-                    {
-                        k++;
-                    }
+                    b = (n-1);
+                    a++;
                 }
             }
-            return k;
+            return array;
         }
     }
 }
