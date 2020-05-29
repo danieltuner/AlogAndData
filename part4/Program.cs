4a71af0b19520fc6351568202a240f63f1dca866
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-29 13:42:57 +0300

    ex 3 done

--- part4/Program.cs
+++ part4/Program.cs
@@ -22,7 +22,7 @@ namespace part4
             Console.WriteLine(g.Last(4)); // 1
             Console.WriteLine(g.Last(123)); // 119
 
-             /*
+            Console.WriteLine();
             SmallestDistance s = new SmallestDistance();
             s.Add(3);
             s.Add(8);
@@ -31,14 +31,14 @@ namespace part4
             Console.WriteLine(s.Calculate()); // 5
             s.Add(9);
             Console.WriteLine(s.Calculate()); // 1
-
+            /*
             Console.WriteLine();
             Order o = new Order();
             int[] a = { 4, 2, 1, 3, 5 };
             int[] b = { 2, 4, 3, 1, 5 };
             int[] c = o.Create(a, b);
             Console.WriteLine(String.Join(" ", c)); // 2 3 5 1 4
-            
+
             Console.WriteLine();
             BinarySearchTree bs = new BinarySearchTree();
             bs.Add(5); // Becomes your root
--- part4/exercise3.cs
+++ part4/exercise3.cs
@@ -1,19 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 
 namespace part4
 {
     public class SmallestDistance
     {
+        List<int> numbers = new List<int>();
         public void Add(int x)
         {
-
+            numbers.Add(x);
         }
 
         public int Calculate()
         {
-            return 0;
-
+            numbers.Sort();
+            int mindiff = numbers[1] - numbers[0];
+            for (int i = 2; i != numbers.Count; i++)
+            {
+                mindiff = Math.Min(mindiff, numbers[i] - numbers[i-1]);
+            }
+            return mindiff;
         }
     }
 }
