35577d88174c4c37b93fa0637498692f7ce1ef29
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-05-29 13:11:11 +0300

    progg

--- part4/Program.cs
+++ part4/Program.cs
@@ -31,11 +31,15 @@ namespace part4
             Console.WriteLine(s.Calculate()); // 5
             s.Add(9);
             Console.WriteLine(s.Calculate()); // 1
+
+            Console.WriteLine();
             Order o = new Order();
             int[] a = { 4, 2, 1, 3, 5 };
             int[] b = { 2, 4, 3, 1, 5 };
             int[] c = o.Create(a, b);
             Console.WriteLine(String.Join(" ", c)); // 2 3 5 1 4
+            
+            Console.WriteLine();
             BinarySearchTree bs = new BinarySearchTree();
             bs.Add(5); // Becomes your root
             bs.Add(4); // Goes to the left
