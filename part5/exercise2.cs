ced3135bcbe951bcb6d21b288ddb41df47bc94c8
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-06-05 11:29:58 +0300

    ex 2 done

--- part5/exercise2.cs
+++ part5/exercise2.cs
@@ -1,24 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 namespace part5
 {
     public class Connectivity
     {
         public int n;
+        public List<int> visited;
+
+        List<int>[] graph;
 
         public Connectivity(int n)
         {
-            this.n = n;
+           this.n = n;
+           
+           this.graph = new List<int>[n + 1];
+           for (int i = 1; i <= n; i++)
+           {
+               graph[i] = new List<int>();
+           }
+           this.visited = new List<int>(); 
         }
 
         public void AddConnection(int a, int b)
         {
-
+            this.graph[a].Add(b);
+            this.graph[b].Add(a);   
         }
 
         public int Calculate(int x)
         {
-            return 0;
+            
+            if (visited.Contains(x))
+            {
+                return 0;
+            }
+            visited.Add(x);
+            foreach (int n in graph[x])
+            {
+                Calculate(n);
+            }
+            return visited.Count -1;
         }
 
     }
