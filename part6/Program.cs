fd256ae32212f5042017ae34b604214b1b7bff3e
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-06-11 09:51:31 +0300

    base

--- part6/Program.cs
+++ part6/Program.cs
@@ -6,7 +6,7 @@ namespace part6
     {
         static void Main(string[] args)
         {
-            ShortestPath s = new ShortestPath(5);
+            BellmanFord s = new BellmanFord(5);
             s.AddRoad(1, 2, 7);
             s.AddRoad(2, 4, 2);
             s.AddRoad(1, 3, 6);
@@ -15,20 +15,22 @@ namespace part6
             Console.WriteLine(s.Calculate(1, 5)); // 12
 
             /*
-            ShortestPath s = new ShortestPath(5);
+            Dijkstra s = new Dijkstra(5);
             s.AddRoad(1, 2, 7);
             s.AddRoad(2, 4, 2);
             s.AddRoad(1, 3, 6);
             s.AddRoad(3, 4, 5);
             s.AddRoad(4, 5, 3);
             Console.WriteLine(s.Calculate(1, 5)); // 12
-            ShortestPath s = new ShortestPath(5);
+
+            FloydWarshall s = new FloydWarshall(5);
             s.AddRoad(1, 2, 7);
             s.AddRoad(2, 4, 2);
             s.AddRoad(1, 3, 6);
             s.AddRoad(3, 4, 5);
             s.AddRoad(4, 5, 3);
             Console.WriteLine(s.Calculate(1, 5)); // 12
+            
             ShortestPath s = new ShortestPath(5);
             s.AddRoad(1, 2, 7);
             s.AddRoad(2, 4, 2);
