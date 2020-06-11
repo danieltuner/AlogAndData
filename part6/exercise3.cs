23d021fccb0e0effdd7cfa2f39746156f48a4b33
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-06-11 09:58:52 +0300

    base

--- part6/exercise3.cs
+++ part6/exercise3.cs
@@ -5,14 +5,17 @@ namespace part6
     public class FloydWarshall
     {
         public int n;
+        public List<Edge> edges;
         public ShortestPath(int n)
         {
             this.n = n;
+            this.edges = new List<Edge>();
         }
 
         public void AddRoad(int a, int b, int d)
         {
-
+            this.edges.Add(new Edge(a, b, d));
+            this.edges.Add(new Edge(b, a, d));
         }
 
         public int Calculate(int x, int y)
