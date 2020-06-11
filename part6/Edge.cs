0b4a9669d53df20ed1293cbac470a5ad95700b45
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-06-11 09:43:50 +0300

    base

---
+++ part6/Edge.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace part6
+{
+    public class Edge
+    {
+        public int beginning, end, weight;
+        public Edge(int beginning, int end, int weight)
+        {
+            this.beginning = beginning;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        
+    }
+}
