477ecb98d1ab6f25dfa41d7dc9259196894b3718
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-03 11:03:38 +0300

    ex1 done

--- Examprep/Program.cs
+++ Examprep/Program.cs
@@ -6,7 +6,10 @@ namespace Examprep
     {
         public static void Main(string[] args)
         {
-
+            Numbers num = new Numbers();
+            Console.WriteLine(num.Steps(11)); // 5
+            Console.WriteLine(num.Steps(150)); // 5
+            Console.WriteLine(num.Steps(2)); // 2
         }
     }
 }
--- Examprep/ex1.cs
+++ Examprep/ex1.cs
@@ -2,8 +2,22 @@ using System;
 
 namespace Examprep
 {
-    public class ex1
+    public class Numbers
     {
+        public int Steps(int x)
+        {
+            int count = 0;
+            int  i = 1;
+            int sum = 0;
 
+            while (sum < x)
+            {
+                sum += i;
+                count++;
+                i++;
+            }
+            return count;
+        }
+  
     }
 }
